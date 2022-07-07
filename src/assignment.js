a0edceee42b8b98fd64059a052a38f8a341240de
Bakare Rilwan Temitope <bakarerilwan1@gmail.com>
2022-07-07 16:40:36 +0000

    Challenge 1 & 2 sopved

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,23 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let length = arrayOfNumbers.length;
+  let i = 0;
+  let sum = 0;
+
+
+
+
+  while (i < length)
+    {
+      sum += arrayOfNumbers[i];
+      i++;
+    }
+return (sum);
+
+
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +42,21 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let length = arrayOfNumbers.length;
+    let i = 0;
+    let evenCount = 0;
+
+  while (i < length)
+    {
+      if (arrayOfNumbers[i] % 2 == 0)
+      {
+        evenCount++;
+      }
+      i++;
+    }
+  return (evenCount);
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
